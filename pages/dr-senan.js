8df83a30e3f18d98801ca62e99e0f5b4baf6474f
muzammal rahim <creative.joomdev@gmail.com>
2021-10-12 11:21:45 +0500

    senan Card changed

--- pages/dr-senan.js
+++ pages/dr-senan.js
@@ -68,39 +68,13 @@ const DoctorDetails = () => {
                     <h3>Biography</h3>
 
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
+                     Senan enjoys all areas of general practice with special interest in
+                     - Diabetes Management
+                     - Cardiovascular health
+                     - 
                     </p>
 
-                    <p>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                      enim ipsam voluptatem quia voluptas sit aspernatur aut
-                      odit aut fugit, sed quia consequuntur magni dolores eos
-                      qui ratione voluptatem sequi nesciunt.
-                    </p>
-
-                    <p>
-                      But I must explain to you how all this mistaken idea of
-                      denouncing pleasure and praising pain was born and I will
-                      give you a complete account of the system, and expound the
-                      actual teachings of the great explorer of the truth, the
-                      master-builder of human happiness. No one rejects,
-                      dislikes, or avoids pleasure itself, because it is
-                      pleasure, but because those who do not know how to pursue
-                      pleasure rationally encounter consequences that are
-                      extremely painful.
-                    </p>
-
-                    <p></p>
+                    
                   </div> */}
 
                   <div className="doctor-details-biography">
@@ -117,10 +91,16 @@ const DoctorDetails = () => {
 
                   <div className="doctor-details-biography">
                     <h3>Experience</h3>
-                    <ul>
+                    {/* <ul>
                       <li> Chronic Disease</li>
                       <li>Diabetes</li>
-                    </ul>
+                    </ul> */}
+                    <p>Senan enjoys all areas of general practice with special interest in <br/>
+                      - Diabetes management<br/>
+                      - Cardiovascular health<br/>
+                      - Geriatric Medicience<br/>
+                      - Emergency Medicine<br/>
+                      - Skin checks and minor procedures for skin cancer</p>
                   </div>
                 </div>
               </div>
